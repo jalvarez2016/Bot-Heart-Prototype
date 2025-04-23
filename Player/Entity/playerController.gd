96e32f7dc21f7a221b80218dd2af91e469c28fa9
jason alvarez <jasonalvarezcoraiza@gmail.com>
2025-04-22 22:51:58 -0400

    basic player movement

---
+++ Player/Entity/playerController.gd
@@ -0,0 +1,49 @@
+extends CharacterBody3D
+@export var gravity := 50.0
+@export var speed := 8.0
+@export var jump_force := 20.0
+@export var health := 100
+
+@onready var spring_arm: SpringArm3D = $Head/SpringArm3D
+@onready var mesh : MeshInstance3D = $MeshInstance3D
+
+var angular_acceleration := 7
+var isAlive : bool = true
+var isSprinting : bool = false
+var dodging : bool = false
+var isOnMachine : bool = false
+
+func _process(delta: float) -> void:
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_force
+	
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
+	var move_direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	move_direction = move_direction.rotated(Vector3.UP, spring_arm.rotation.y).normalized()
+	
+	
+	if move_direction:
+		# sprinting
+		if Input.is_action_pressed("sprint"):
+			isSprinting = true
+			velocity.x = move_direction.x * speed * 1.8
+			velocity.z = move_direction.z * speed * 1.8
+			
+		# walking
+		else:
+			isSprinting = false
+			velocity.x = move_direction.x * speed
+			velocity.z = move_direction.z * speed
+		mesh.rotation.y = lerp_angle(mesh.rotation.y, atan2(move_direction.x, move_direction.z), delta * angular_acceleration)
+		
+	# standing still
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+	
+	move_and_slide()
